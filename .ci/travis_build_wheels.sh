0bf7c51fce9a6d735e54ad881a8bb29b02dc91f9
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-03-25 16:25:34 +0000

    Disable manylinux1 i686 build due to it not working with matplotlib

--- .ci/travis_build_wheels.sh
+++ .ci/travis_build_wheels.sh
@@ -2,19 +2,17 @@
 set -e -x
 
 # Compile wheels
-
 PYBINS=(
   "/opt/python/cp36-cp36m/bin"
   "/opt/python/cp37-cp37m/bin"
 #  "/opt/python/cp38-cp38m/bin"
   )
 
-# and later...
 for PYBIN in ${PYBINS[@]}; do
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/pyms_nist_search*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
