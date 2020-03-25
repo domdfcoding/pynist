f2ea9987befe05522967b04e63ca0eb207421daa
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-03-25 17:03:51 +0000

    Update .travis.yml to deploy to PyPI

--- .ci/travis_deploy.sh
+++ .ci/travis_deploy.sh
@@ -1,3 +1,8 @@
-echo "Deploying to PyPI..."
-pip3 install twine
-twine upload  -u DomDF -p ${pypi_password} --skip-existing *.whl
+if [ -z "$var" ];
+then
+    echo "Skipping deploy because this is not a tagged commit"
+else
+    echo "Deploying to PyPI..."
+    pip3 install twine
+    twine upload  -u DomDF -p ${pypi_password} --skip-existing wheelhouse/*.whl
+fi
