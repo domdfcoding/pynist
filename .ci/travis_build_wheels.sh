123e29d2276de95468c5637a52eedfff229b92d4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-03-25 16:03:54 +0000

    Removed reference to dev-requirements.txt in travis_build_wheels.sh

--- .ci/travis_build_wheels.sh
+++ .ci/travis_build_wheels.sh
@@ -6,7 +6,7 @@ yum install -y atlas-devel
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+#    "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
