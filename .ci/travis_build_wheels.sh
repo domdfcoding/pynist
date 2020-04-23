31099aac951887779fcc0ff179fe0cdce1c17057
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-23 21:38:28 +0100

    Trying to fix Travis build (2)

--- .ci/travis_build_wheels.sh
+++ .ci/travis_build_wheels.sh
@@ -20,13 +20,13 @@ for PYVERSION in ${PYVERSIONS[@]}; do
     ls wheelhouse
 
     # Bundle external shared libraries into the wheels
-    for whl in wheelhouse/pyms_nist_search-${PYVERSION}*.whl; do
+    for whl in wheelhouse/pyms_nist_search-*-${PYVERSION}*.whl; do
          "/opt/python/${PYVERSION}/bin/python" -m auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
     done
 
     # Install pyms_nist_search and pytest
     "${PYBIN}/pip" install pytest
-    "${PYBIN}/pip" install wheelhouse/pyms_nist_search-${PYVERSION}-manylinux*_x86_64.whl
+    "${PYBIN}/pip" install wheelhouse/pyms_nist_search-*-${PYVERSION}-manylinux*_x86_64.whl
 
     # Run pytest
     "${PYBIN}/python" -m pytest
