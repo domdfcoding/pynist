a19a66431118cb548d6bef4ac624232b3bc74ada
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-23 22:12:36 +0100

    Trying to fix Travis build (4)

--- .ci/travis_build_wheels.sh
+++ .ci/travis_build_wheels.sh
@@ -11,6 +11,8 @@ PYVERSIONS=(
   #"cp38-cp38m"
   )
 
+export VERSION_NO="0.4.11"
+
 for PYVERSION in ${PYVERSIONS[@]}; do
 
     export PYBIN="/opt/python/${PYVERSION}/bin"
@@ -18,21 +20,20 @@ for PYVERSION in ${PYVERSIONS[@]}; do
     # Upgrade auditwheel to fix borked docker image from 26 Mar 2020
     "${PYBIN}/pip" install auditwheel --upgrade
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    "${PYBIN}/pip" install pytest
 
     # for diagnostics
     ls wheelhouse
 
     # Bundle external shared libraries into the wheels
-    for whl in wheelhouse/pyms_nist_search-*-${PYVERSION}*.whl; do
+    for whl in wheelhouse/pyms_nist_search-${VERSION_NO}-${PYVERSION}*.whl; do
          "${PYBIN}/python" -m auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+
+         # Install pyms_nist_search and test
+         "${PYBIN}/pip" install "$whl"
+         "${PYBIN}/python" -m pytest
+         # TODO: coverage with coverage, pytest-cov and coveralls, then upload to coveralls
     done
 
-    # Install pyms_nist_search and pytest
-    "${PYBIN}/pip" install pytest
-    "${PYBIN}/pip" install wheelhouse/pyms_nist_search-*-${PYVERSION}-manylinux*_x86_64.whl
-
-    # Run pytest
-    "${PYBIN}/python" -m pytest
-    # TODO: coverage with coverage, pytest-cov and coveralls, then upload to coveralls
 done
 
