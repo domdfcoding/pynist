6fa72ad696e91f3dce076548ee900c291c05194f
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-20 22:33:33 +0100

    Moving manylinux build process from Travis to GitHub Actions

--- .ci/travis_deploy.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$TRAVIS_TAG" ];
-then
-    echo "Skipping deploy because this is not a tagged commit"
-else
-    echo "Deploying to PyPI..."
-    pip3 install twine
-    twine upload  -u DomDF -p ${pypi_password} --skip-existing wheelhouse/*.whl
-fi
