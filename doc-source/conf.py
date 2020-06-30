da732b74c71fc11fea05993fe9a58a28e046b8d4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-30 18:57:11 +0000

    Updated files with `git_helper`.

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -86,7 +86,7 @@ html_show_sourcelink = False  # True will show link to source
 html_context = {
 		'display_github': True,
 		'github_user': 'domdfcoding',
-		'github_repo': 'cawdrey',
+		'github_repo': 'pynist',
 		'github_version': 'master',
 		'conf_py_path': '/',
 		}
