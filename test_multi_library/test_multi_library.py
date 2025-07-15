f09ddf9223e14cf7dc136c6860fb98bc1ca5b51c
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-07-15 16:26:38 +0100

    Add more tests

--- test_multi_library/test_multi_library.py
+++ test_multi_library/test_multi_library.py
@@ -15,10 +15,13 @@ repo_root = pathlib.Path(__file__).parent.parent.absolute()
 FULL_PATH_TO_MONA_LIBRARY = str(repo_root / "MoNA_GCMS_Library" / "MoNA")
 FULL_PATH_TO_C1_LIBRARY = str(repo_root / "test_multi_library" / "c1")
 FULL_PATH_TO_C2_LIBRARY = str(repo_root / "test_multi_library" / "c2")
+FULL_PATH_TO_C3_LIBRARY = str(repo_root / "test_multi_library" / "c3")
+FULL_PATH_TO_C4_LIBRARY = str(repo_root / "test_multi_library" / "c4")
+FULL_PATH_TO_C5_LIBRARY = str(repo_root / "test_multi_library" / "c5")
 FULL_PATH_TO_WORK_DIR = str(repo_root)
 
 
-def test_mona_and_extra_lib():
+def test_c1_c2_51():
 	with pyms_nist_search.Engine(
 			[
 					(FULL_PATH_TO_C1_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
@@ -35,13 +38,92 @@ def test_mona_and_extra_lib():
 		hit_list = engine.full_spectrum_search(spectrum, n_hits=5)
 		hit_list_names = [hit.name for hit in hit_list]
 	
-		# assert len(hit_list) == 5
+		assert len(hit_list) == 2
 
 		for hit in hit_list:
 			# TODO: test CAS numbers
 			assert isinstance(hit, SearchResult)
 
+		print(hit_list)
+		print(hit_list_names)
 		assert "1-NITROPYRENE" in hit_list_names
+		assert "2,4-DINITROPHENOL" in hit_list_names
+		assert "3,4-DICHLOROPHENOL" not in hit_list_names
+		assert "2,5-DICHLOROPHENOL" not in hit_list_names
+		assert "2,6-DICHLOROPHENOL" not in hit_list_names
+
+
+
+def test_all_51():
+	with pyms_nist_search.Engine(
+			[
+					(FULL_PATH_TO_C1_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C2_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C3_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C4_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C5_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					],
+			work_dir=FULL_PATH_TO_WORK_DIR,
+			) as engine:
+
+		print()
+
+		spectrum = MassSpectrum([51.0], [27])
+
+		
+		hit_list = engine.full_spectrum_search(spectrum, n_hits=5)
+		hit_list_names = [hit.name for hit in hit_list]
+	
+		assert len(hit_list) == 4
+
+		for hit in hit_list:
+			# TODO: test CAS numbers
+			assert isinstance(hit, SearchResult)
+
+		print(hit_list)
+		print(hit_list_names)
+		assert "1-NITROPYRENE" in hit_list_names
+		assert "2,4-DINITROPHENOL" in hit_list_names
+		assert "3,4-DICHLOROPHENOL" in hit_list_names
+		assert "2,5-DICHLOROPHENOL" in hit_list_names
+		assert "2,6-DICHLOROPHENOL" not in hit_list_names
+
+
+
+def test_all_53():
+	with pyms_nist_search.Engine(
+			[
+					(FULL_PATH_TO_C1_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C2_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C3_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C4_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					(FULL_PATH_TO_C5_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+					],
+			work_dir=FULL_PATH_TO_WORK_DIR,
+			) as engine:
+
+		print()
+
+		spectrum = MassSpectrum([53.0], [64])
+
+		
+		hit_list = engine.full_spectrum_search(spectrum, n_hits=5)
+		hit_list_names = [hit.name for hit in hit_list]
+	
+		assert len(hit_list) == 3
+
+		for hit in hit_list:
+			# TODO: test CAS numbers
+			assert isinstance(hit, SearchResult)
+
+		print(hit_list)
+		print(hit_list_names)
+		assert "1-NITROPYRENE" not in hit_list_names
+		assert "2,4-DINITROPHENOL" not in hit_list_names
+		assert "3,4-DICHLOROPHENOL" in hit_list_names
+		assert "2,5-DICHLOROPHENOL" in hit_list_names
+		assert "2,6-DICHLOROPHENOL" in hit_list_names
+
 
 
 # def test_different_n_hits(search: pyms_nist_search.Engine, spectra: Tuple[str, Optional[MassSpectrum]]):
