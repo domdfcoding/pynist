29c5d8b8cf69d6c4c270a0ade1aaf066e5a1fa4e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-07-15 10:31:43 +0100

    Further WIP support for multiple libraries.

---
+++ test_multi_library/test_multi_library.py
@@ -0,0 +1,60 @@
+# stdlib
+import pathlib
+from typing import Optional, Tuple
+
+# 3rd party
+import pytest
+from pyms.Spectrum import MassSpectrum
+
+# this package
+import pyms_nist_search
+from pyms_nist_search import SearchResult
+
+
+repo_root = pathlib.Path(__file__).parent.parent.absolute()
+
+FULL_PATH_TO_MONA_LIBRARY = str(repo_root / "MoNA_GCMS_Library" / "MoNA")
+FULL_PATH_TO_C1_LIBRARY = str(repo_root / "test_multi_library" / "c1")
+FULL_PATH_TO_C2_LIBRARY = str(repo_root / "test_multi_library" / "c2")
+FULL_PATH_TO_WORK_DIR = str(repo_root)
+
+
+def test_mona_and_extra_lib(spectra: Tuple[str, Optional[MassSpectrum]]):
+	with pyms_nist_search.Engine(
+			[
+				# (FULL_PATH_TO_MONA_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+				(FULL_PATH_TO_C1_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+				(FULL_PATH_TO_C2_LIBRARY, pyms_nist_search.NISTMS_USER_LIB),
+	],
+			work_dir=FULL_PATH_TO_WORK_DIR,
+			) as engine:
+
+		
+		print()
+
+		name, spectrum = spectra
+		print(f"Testing {name}")
+		hit_list = engine.full_spectrum_search(spectrum, n_hits=5)
+
+		assert len(hit_list) == 5
+
+		for hit in hit_list:
+			# TODO: test CAS numbers
+			assert isinstance(hit, SearchResult)
+
+		assert hit_list[0].name.lower() == name.lower()
+		# assert hit_list[0].cas == cas
+
+
+# def test_different_n_hits(search: pyms_nist_search.Engine, spectra: Tuple[str, Optional[MassSpectrum]]):
+# 	print()
+
+# 	name, spectrum = spectra
+# 	if name != "Diphenylamine":
+# 		pytest.skip()
+
+# 	for n_hits in range(1, 21):
+# 		print(f"Testing with {n_hits} hits")
+# 		hit_list = search.full_spectrum_search(spectrum, n_hits=n_hits)
+
+# 		assert len(hit_list) == n_hits
